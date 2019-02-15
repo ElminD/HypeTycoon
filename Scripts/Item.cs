e38e29514b5d6e11a0dc3e8cd591454c3c786d21
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2019-02-15 11:51:46 -0600

    Added Scripts 2/15/2019
    Added Money and Item Scripts

---
+++ Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour {
+
+    public int Cost;
+    public bool itemBought = false;
+    public int Boost;
+
+    
+    
+    public void ButtonClick()
+    {
+        if (Money.MoneyAmount > Cost)
+        {
+            Money.MoneyAmount = Money.MoneyAmount - Cost;
+
+            itemBought = true;
+
+            if (itemBought = true)
+            {
+                Money.MoneyPerSec = Money.MoneyPerSec + Boost;
+            }
+
+        }
+
+        
+    }
+
+    
+
+    
+}
