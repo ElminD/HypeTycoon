447b0f31fcf2eee306045ef3400e7c3eebd41da9
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2019-02-20 11:12:48 -0600

    ui scroller
    ui scroller

---
+++ HypeTycoon/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Camera CameraMain;
+    private int speed = 10;
+    public double x = 12.86;
+
+    
+    public void ButtonClickRight()
+    {
+
+        CameraMain.transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+    }
+
+    public void ButtonClickLeft()
+    {
+        if (CameraMain.transform.position.x != x)
+        {
+            CameraMain.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
